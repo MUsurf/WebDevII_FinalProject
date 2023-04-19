1130585669ec94fe976b4ce81b30cb0dbb706fa0
MCLusardi <mclntg@mail.missouri.edu>
2023-04-19 12:24:01 -0500

    implemented basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { MediaComponent } from './media/media.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: HomeComponent
+  },
+  {
+    path:'news',component: NewsComponent
+  },
+  {
+    path:'media',component:MediaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
