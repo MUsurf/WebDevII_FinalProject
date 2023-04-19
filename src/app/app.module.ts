0afa2faf8d158dae1a594cc13661309ce2b9674e
MCLusardi <mclntg@mail.missouri.edu>
2023-04-19 10:10:30 -0500

    generated required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { MediaComponent } from './media/media.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NewsComponent,
+    MediaComponent
   ],
   imports: [
     BrowserModule,
