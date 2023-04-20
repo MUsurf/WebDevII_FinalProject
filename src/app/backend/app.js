424eb7bc8716bf2b7e64029c33c335a55963b257
Melissa Tully <mjt85d@umsystem.edu>
2023-04-20 12:20:44 -0500

    trying to add mongo & mongoose

--- src/app/backend/app.js
+++ src/app/backend/app.js
@@ -2,9 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const HomeModel = require('./models/Officer')
+const ArticleModel = require('./models/Article')
+const PhotoModel = require('./models/Photo')
+//home, news, media
+
+mongoose.connect('mongodb+srv://SURF_Webmaster:2MQjduCM4U9q7eGx@mizzousurf.l9qioaf.mongodb.net/test')
+  .then(()=>{
+    console.log('Connected to database')
+  })
+  .catch(()=>{
+    console.log('connection error')
+})
+
+
 app.get('/', (req, res) => {
 
 })
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// put in mongoose
+// just app.get
